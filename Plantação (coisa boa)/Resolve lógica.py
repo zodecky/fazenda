782517d1fac02a0180cba1dcfbc7034252927255
zodecky <103199986+zodecky@users.noreply.github.com>
2022-05-18 12:34:58 -0300

    Atualização logica
    Dicionario adicionado,

--- Plantação (coisa boa)/Resolve lógica.py
+++ Plantação (coisa boa)/Resolve lógica.py
@@ -16,27 +16,30 @@ def removeCaracter_r(palavra, caracter):
     return removeCaracter_r(palavra[1:], caracter)
 
 
-"""Função que checa se os parêntesis estão balanceados"""
+"""Função que checa se os parêntesis estão balanceados e retorna os pares de parêntesis"""
+def encontra_parens(formula):
+    tabela = {} # Dicionario q retorna os pares
+    stack_indices = [] # Guarda temporariamente o índice de cada
 
+    for i, c in enumerate(formula):
+        if c == '(':
+            stack_indices.append(i)
+        elif c == ')':
+            if len(stack_indices) == 0:
+                raise IndexError("No matching closing parens at: " + str(i))
+            tabela[stack_indices.pop()] = i
 
-def teste_parentesis(myStr):
-    stack = []
-    open_list = ["{", "[", "("]
-    close_list = ["}", "]", ")"]
-    for i in myStr:
-        if i in open_list:
-            stack.append(i)
-        elif i in close_list:
-            pos = close_list.index(i)
-            if ((len(stack) > 0) and (open_list[pos] == stack[len(stack) - 1])):
-                stack.pop()
-            else:
-                return "Parêntesis desbalanceados!"
-    if len(stack) == 0:
-        return "Parêntesis balanceados!"
-    else:
-        return "Parêntesis desbalanceados!"
+    if len(stack_indices) > 0:
+        raise IndexError("No matching opening parens at: " + str(stack_indices.pop()))
 
+    return tabela
+
+def get_key(my_dict, val):
+    for key, value in my_dict.items():
+         if val == value:
+             return key
+ 
+    raise IndexError("Key doesn't exist")
 
 def separa_formula(formula: str) -> list:
 
@@ -49,28 +52,40 @@ def separa_formula(formula: str) -> list:
     #   Se o valor - 1 (anda 1 casas para frente) é ∃ ou ∀
     #   Caso verdade:
     #       retornar o tipo, o operador, a formula e o sinal (verdadeiro/falso).
-    if formula[index_abre + 1] == "∀" or formula[index_abre + 1] == "∃":
+    if (
+        
+        (formula[index_abre + 1] == "∀" or formula[index_abre + 1] == "∃") and
+        formula[index_abre + 3] == "(" 
+        
+        ):
+        # Saída:
         return {"operador": formula[index_abre + 1:index_abre + 3], "sinal": formula[0], "formula": formula[index_abre + 3:-2]}
 
     # Caso falso:
     #   retornar o tipo, a primeira parte da formula, o operador,
     #   a segunda parte da formula e o sinal.
     else:
-        pass
+        tabela_parentesis = encontra_parens(formula)
+        index_abre_1 = min(tabela_parentesis.keys())
+        index_fecha_2 = max(tabela_parentesis.values())
+
+        index_fecha_1 = tabela_parentesis[index_abre_1]
+        index_abre_2 = get_key(tabela_parentesis, index_fecha_2)
+
+        print(index_abre_1, index_fecha_1, index_abre_2, index_fecha_2)
+        print(tabela_parentesis)
 
         
 # - - - - - - - - - - - - LOGICA DO RETORNO - - - - - - - - - - - - - #
 
-    print(f"Entrada: {index_abre}, Saída: {index_fecha}")
-
-
 def main():
+
     txt = "F(∃y(∀xP(x)→P(y)))"
     txt2 = "F(∀x(P(x)∧Q(x))→(∀xP(x)∧∀xQ(x)))"
     txt3 = "F(P(a)∧Q(a))→(∀xP(x)∧∀xQ(x))"
 
-    print(separa_formula(txt))
-
+    separa_formula(txt3)
 
 if __name__ == '__main__':
     main()
+    
--- teste.py
+++ teste.py
@@ -1,39 +1,18 @@
-"""Função só aceita parêntesis balanceados!"""
-def achaParentesis(formula):
+def encontra_parens(formula):
+    tabela = {} # Dicionario q retorna os pares
+    stack_indices = [] # Guarda temporariamente o índice de cada
 
-    i = 0 # Contador de loops
-    lista_index_abertura = [] # Lista com os index de abertura
-    lista_index_fechamento = [] # Lista com os index de fechamento
-
-    while formula != "":
-        if formula[0] == "(":
-            lista_index_abertura.append(i)
-        if formula[0] == ")":
-            lista_index_fechamento.append(i)
-        
-        formula = formula[1:] # Remove letra da formula
-        i += 1 #incrementa loop
-
-    print(lista_index_abertura, lista_index_fechamento)
-
-achaParentesis("(P(a)∧Q(a))→(∀xP(x)∧∀xQ(x))")
-
-
-def find_parens(s):
-    toret = {}
-    pstack = []
-
-    for i, c in enumerate(s):
+    for i, c in enumerate(formula):
         if c == '(':
-            pstack.append(i)
+            stack_indices.append(i)
         elif c == ')':
-            if len(pstack) == 0:
+            if len(stack_indices) == 0:
                 raise IndexError("No matching closing parens at: " + str(i))
-            toret[pstack.pop()] = i
+            tabela[stack_indices.pop()] = i
 
-    if len(pstack) > 0:
-        raise IndexError("No matching opening parens at: " + str(pstack.pop()))
+    if len(stack_indices) > 0:
+        raise IndexError("No matching opening parens at: " + str(stack_indices.pop()))
 
-    return toret
+    return tabela
 
-print(find_parens("(P(a)∧Q(a))→(∀xP(x∧∀xQ(x))"))
+print(encontra_parens("(P(a)∧Q(a))→(∀xP(x∧∀xQ(x))"))
