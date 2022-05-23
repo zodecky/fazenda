22d5bde6835c7bed97a211e643ca58d83eae1b0d
zodecky <103199986+zodecky@users.noreply.github.com>
2022-05-23 12:15:31 -0300

    aaaa

--- Galinheiro/testes_arquivos.py
+++ Galinheiro/testes_arquivos.py
@@ -1,5 +1,14 @@
-arq = open("Galinheiro/arquivo.txt", "r")
+def lista_aprovados(nome):
+    arq = open("Galinheiro/arquivo.txt", "r")
 
-for linha in arq:
-    print(linha, end="")
-arq.close()
+    for linha in arq:
+        p1 = linha.find(",")
+        nome = linha[:p1]
+
+        p2 = linha.find(",", p1 + 1)
+        n1 = float(linha[p1 + 1:p2])
+        n2 = float(linha[p2 + 1:])
+
+        if (n1 + n2) / 2 >= 5.0:
+            print(nome)
+    arq.close()
